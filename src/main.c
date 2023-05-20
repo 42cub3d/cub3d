d0b83cc90d37c9e8059629a1410d658f9cb91136
edwin9905 <qkr990509@naver.com>
2023-05-20 21:42:25 +0900

    [feat]: valid map error fixed

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: gkwon <gkwon@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/29 17:01:47 by subcho            #+#    #+#             */
-/*   Updated: 2023/05/20 20:30:53 by gkwon            ###   ########.fr       */
+/*   Updated: 2023/05/20 21:40:50 by gkwon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,29 +36,35 @@ int	valid_argv(char **argv)
 	return (0);
 }
 
-int	map_valid_check(t_map *map, unsigned int i, unsigned int j)
+int	map_valid_check(t_map *map, unsigned int std, unsigned int j)
 {
-	while (map->map_char[++i])
+	unsigned int	i;
+
+	i = 0;
+	while (i < map->y)
 	{
-		j = -1;
-		while (map->map_char[i][++j])
+		j = 0;
+		while (map->map_char[i + std][j] != '\n' && map->map_char[i + std][j] != '\0')
 		{
-			if ((i == 0 || i == map->y) || (j == 0 || j == ft_strlen(map->map_char[i])))
-				if (map->map_char[i][j] != 1)
+			if ((i == 0 || i == map->y) || (j == 0 || j == ft_strlen(map->map_char[i + std])))
+				if (map->map_char[i + std][j] != '1')
 					return (1);
-			if (map->map_char[i][j] == 'N' ||
-				map->map_char[i][j] == 'S' ||
-				map->map_char[i][j] == 'E' ||
-				map->map_char[i][j] == 'W')
+			if (map->map_char[i + std][j] == 'N' ||
+				map->map_char[i + std][j] == 'S' ||
+				map->map_char[i + std][j] == 'E' ||
+				map->map_char[i + std][j] == 'W')
 			{
 				map->player.x = j;
 				map->player.y = i;
 				map->is_player_in_map = 1;
+				printf("x : %d y : %d", j, i);
 			}
-			else if (map->map_char[i][j] != '\n' && map->map_char[i][j] != '0'
-					&& map->map_char[i][j] != '1')
+			else if (map->map_char[i + std][j] != '\n' && map->map_char[i + std][j] != '0'
+					&& map->map_char[i + std][j] != '1')
 				return (0);
+			j++;
 		}
+		i++;
 	}
 	if (map->is_player_in_map != 1)
 		return (1);
@@ -78,7 +84,7 @@ int	main(int argc, char **argv)
 		ft_error(E_FD);
 	init_map(&map, map.fd, 0);
 	search_arg(&map, map.y -1, 0);
-	if (!map_valid_check(&map, -1, -1))
+	if (map_valid_check(&map, 6, -1))
 		ft_error(E_MAP_COLSED);
 	map.mlx = mlx_init();
 	if (!map.mlx)
