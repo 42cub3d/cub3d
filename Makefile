ecc2c41af135f63336c276c3e2b3fffd382b90a1
chobinee <tnqls991213@icloud.com>
2023-07-13 21:31:45 +0900

    fit: set direction

--- Makefile
+++ Makefile
@@ -5,15 +5,16 @@ LIBFT=./libft/libft.a
 HEADER=-I./header
 NAME=cub3D
 SRCS=./src/main.c\
-		./src/str_utils.c\
-		./src/key_hook.c\
 		./src/draw_map.c\
+		./src/str_utils.c\
 		./src/map_parsing_init.c\
-		./src/cub3d_utils.c\
-		./src/raycasting.c\
+		./src/key_hook.c\
 		./src/key_press.c\
-		./src/raycasting2.c\
+		./src/cub3d_utils.c\
 		./src/cub3d_utils2.c\
+		./src/cub3d_utils3.c\
+		./src/raycasting.c\
+		./src/raycasting2.c\
 		./src/raycasting3.c
 OBJS=$(SRCS:.c=.o)
 
--- src/raycasting3.c
+++ src/raycasting3.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   raycasting3.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gkwon <gkwon@student.42.fr>                +#+  +:+       +#+        */
+/*   By: subcho <subcho@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/12 17:23:58 by gkwon             #+#    #+#             */
-/*   Updated: 2023/07/12 17:25:55 by gkwon            ###   ########.fr       */
+/*   Updated: 2023/07/13 21:29:54 by subcho           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,12 @@ int	set_tex_num(t_map *map, double ray_dir_x, double ray_dir_y)
 	int	tex_num;
 
 	if (map->dda->side == 1 && ray_dir_y > 0)
-		tex_num = 0;
-	if (map->dda->side == 1 && ray_dir_y < 0)
-		tex_num = 1;
-	if (map->dda->side == 0 && ray_dir_x > 0)
 		tex_num = 2;
-	if (map->dda->side == 0 && ray_dir_x < 0)
+	if (map->dda->side == 1 && ray_dir_y < 0)
 		tex_num = 3;
+	if (map->dda->side == 0 && ray_dir_x > 0)
+		tex_num = 0;
+	if (map->dda->side == 0 && ray_dir_x < 0)
+		tex_num = 1;
 	return (tex_num);
 }
